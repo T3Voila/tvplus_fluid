9db9b0a4c6aeb6702433146d886721cf35efdcb1
Alexander Opitz <opitz@extrameile.agency>
2022-05-22 14:08:46 +0200

    Add fluid as required extension

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,6 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.5.99',
+            'fluid' => '10.4.0-11.5.99',
             'templavoilaplus' => '8.0.3-',
         ],
     ],
