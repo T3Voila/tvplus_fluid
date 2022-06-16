2eec5e2b748de8c7ce5daad40f3dcc1da846ac53
Alexander Opitz <opitz.alexander@googlemail.com>
2022-06-16 20:48:12 +0200

    [RELEASE] Releasing 0.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,14 +5,14 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Opitz',
     'author_email' => 'opitz@extrameile-gehen.de',
     'author_company' => 'Extrameile GmbH',
-    'version' => '0.1.0',
+    'version' => '0.2.0',
     'state' => 'alpha',
     'clearcacheonload' => 1,
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-11.5.99',
             'fluid' => '8.7.0-11.5.99',
-            'templavoilaplus' => '8.0.3-',
+            'templavoilaplus' => '8.0.4-',
         ],
     ],
 ];
