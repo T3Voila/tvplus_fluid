67a809c16c8eca7e02a2f162f66f8cfab761aa26
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2023-08-02 10:36:30 +0200

    [TASK] replace old constant TYPO3_MODE

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') || die();
 
 // @TODO This line can be removed after cache is implemented
 \Tvp\TemplaVoilaPlus\Utility\ExtensionUtility::registerExtension('tvplus_fluid');
